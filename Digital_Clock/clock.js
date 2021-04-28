23f169ee0772fa47d9a9dd1b1215c557b981aa49
edolly2 <edolly2@protonmail.com>
2021-04-28 00:47:24 -0500

    ""

--- Digital_Clock/clock.js
+++ Digital_Clock/clock.js
@@ -29,11 +29,4 @@ function showTime(){
 showTime();
 
 
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0');
-var yyyy = today.getFullYear();
 
-today = mm + '/' + dd + '/' + yyyy;
-document.write(today);
-console.log(today);
